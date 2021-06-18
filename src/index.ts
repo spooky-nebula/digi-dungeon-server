8e24253afe7acab27f14594509551e6ff6c29f40
Pedro Pires <plpires99@gmail.com>
2021-06-18 13:02:41 +0100

    Steps into separating the client and the server

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,36 @@ const io = new socketIO.Server(server);
 
 app.get("/", (req: express.Request, res: express.Response) => {
     res.status(200).json({
-        message: "Socket Server for Digi-Dungeon, please use the correct client to connect to the server",
+        message: "Socket Server for Digi-Dungeon, please use the a client to connect to the server",
         status: "Online"
     });
 });
 
+app.get("/cdn", (req: express.Request, res: express.Response) => {
+    res.status(404).json({
+        message: "CDN not yet implemented",
+        status: "Offline"
+    })
+});
+
+app.post("/register", (req: express.Request, res: express.Response) => {
+    
+});
+
+app.post("/login", (req: express.Request, res: express.Response) => {
+    
+});
+
+app.post("/logout", (req: express.Request, res: express.Response) => {
+    
+});
+
 io.on("connection", (socket: socketIO.Socket) => {
     console.log(socket.id);
+
+    socket.on("handshake", (data: any) => {
+
+    });
 });
 
 app.listen(8080, () => {
