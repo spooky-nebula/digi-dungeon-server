5db360b336aaf99b9773028175229796ba7f4f31
onelikeandidie <plpires99@gmail.com>
2021-08-10 20:48:51 +0100

    Moved all the server stuff to a class

--- src/index.ts
+++ src/index.ts
@@ -12,92 +12,111 @@ import CDN from './core/cdn';
 import Authentication from './core/authentication';
 import WebAPI from './core/webapi';
 
-//#region Config and Initialization
-dotenv.config();
+class DigiDungeonServer {
+  app: express.Express;
+  server: http.Server;
+  io: socketIO.Server;
+  PORT: number | string;
 
-const app = express();
-app.use(cors());
-const server = http.createServer(app);
-const io = new socketIO.Server(server, {
-  cors: {
-    origin: /\\https?:\/\/(.*)|(127\.0\.0\.1)|(localhost):3000/,
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['digi-dungeon-server'],
-    credentials: true
-  }
-});
+  constructor() {
+    //#region Config and Initialization
+    dotenv.config();
 
-const PORT = 8080;
-
-Database.init();
-Communications.init(io);
-Authentication.init();
-CDN.init();
-WebAPI.init();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-//#endregion
-
-//#region HTTP POST/GET STUFF
-
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.status(200).json({
-    message:
-      'Socket Server for Digi-Dungeon, please use the a client to connect to the server',
-    status: 'Online'
-  });
-});
-
-// CDN Related
-app.get('/cdn', CDN.GET_cdn);
-
-// Web API Related
-app.get('/api/user', WebAPI.GET_apiUser);
-
-// Authentication Related
-app.post('/register', Authentication.POST_register);
-app.post('/login', Authentication.POST_login);
-app.post('/logout', Authentication.POST_logout);
-
-//#endregion
-
-//#region Socket IO Stuff
-
-io.on('connection', (socket: socketIO.Socket) => {
-  socket.on('handshake', (data: ddapi.Auth.Handshake.HandshakeData) => {
-    Communications.handshakeHandler(socket, data);
-  });
-
-  socket.on('board-event', (eventData: ddapi.Event.default) => {
-    Communications.boardEventHandler(socket, eventData);
-  });
-
-  socket.on('disconnect', () => {
-    Communications.disconnectHandler(socket);
-  });
-});
-
-//#endregion
-
-//#region Start and Finish
-server.listen(PORT, () => {
-  console.log('Listening on port:', PORT);
-});
-
-let exiting = false;
-
-function exitGracefully() {
-  if (!exiting) {
-    exiting = true;
-    console.info('\nExiting Gracefully.');
-    Database.dispose().then(() => {
-      process.exit(0);
+    this.app = express();
+    this.app.use(cors());
+    this.server = http.createServer(this.app);
+    this.io = new socketIO.Server(this.server, {
+      cors: {
+        origin: /\\https?:\/\/(.*)|(127\.0\.0\.1)|(localhost):3000/,
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['digi-dungeon-server'],
+        credentials: true
+      }
     });
+
+    this.PORT = process.env.SERVER_PORT || 8080;
+
+    Database.init();
+    Communications.init(this.io);
+    Authentication.init();
+    CDN.init();
+    WebAPI.init();
+
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    //#endregion
   }
+
+  start() {
+    //#region HTTP POST/GET STUFF
+    this.app.get('/', (req: express.Request, res: express.Response) => {
+      res.status(200).json({
+        message:
+          'Socket Server for Digi-Dungeon, please use the a client to connect to the server',
+        status: 'Online'
+      });
+    });
+
+    // CDN Related
+    this.app.get('/cdn', CDN.GET_cdn);
+
+    // Web API Related
+    this.app.get('/api/user', WebAPI.GET_apiUser);
+
+    // Authentication Related
+    this.app.post('/register', Authentication.POST_register);
+    this.app.post('/login', Authentication.POST_login);
+    this.app.post('/logout', Authentication.POST_logout);
+    //#endregion
+
+    //#region Socket IO Stuff
+
+    this.io.on('connection', (socket: socketIO.Socket) => {
+      socket.on('handshake', (data: ddapi.Auth.Handshake.HandshakeData) => {
+        Communications.handshakeHandler(socket, data);
+      });
+
+      socket.on('board-event', (eventData: ddapi.Event.default) => {
+        Communications.boardEventHandler(socket, eventData);
+      });
+
+      socket.on('disconnect', () => {
+        Communications.disconnectHandler(socket);
+      });
+    });
+
+    //#endregion
+
+    //#region Start and Finish
+    this.server.listen(this.PORT, () => {
+      console.log('Listening on port:', this.PORT);
+    });
+
+    let exiting = false;
+
+    function exitGracefully() {
+      if (!exiting) {
+        exiting = true;
+        console.info('\nExiting Gracefully.');
+        Database.dispose().then(() => {
+          process.exit(0);
+        });
+      }
+    }
+
+    process.on('SIGINT', exitGracefully);
+    process.on('SIGTERM', exitGracefully);
+    process.on('SIGHUP', exitGracefully);
+    //#endregion
+  }
+
+  run = this.start;
 }
 
-process.on('SIGINT', exitGracefully);
-process.on('SIGTERM', exitGracefully);
-process.on('SIGHUP', exitGracefully);
-//#endregion
+export default DigiDungeonServer;
+export { DigiDungeonServer, DigiDungeonServer as dds };
+
+if (typeof require !== 'undefined' && require.main === module) {
+  let dds = new DigiDungeonServer();
+  dds.start();
+}
