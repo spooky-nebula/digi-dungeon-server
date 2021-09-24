de5c2885239b9e874bb1bf8be15c3fb4bc2b966c
onelikeandidie <onelikeandidie@gmail.com>
2021-09-24 17:54:49 +0100

    First attempt at protobuf

--- src/core/authentication.ts
+++ src/core/authentication.ts
@@ -8,11 +8,35 @@ import { saltPassword } from '../util/cryptography';
 import { sanitize } from '../util/stringExtensions';
 import { random } from '../util';
 
+import * as protobuf from 'protobufjs';
+import path from 'path';
+
 export default class Authentication {
   static logPrefix: string;
+  static protoRoot: protobuf.Root;
 
   static init() {
     this.logPrefix = '[Authentication]';
+    protobuf
+      .load(
+        path.join(
+          __dirname,
+          '../../node_modules/digi-dungeon-protobuf/src/auth/userdata.proto'
+        )
+      )
+      .then((value) => {
+        this.protoRoot = value;
+      });
+  }
+
+  static encodeResponse(lookupType: string, message: any) {
+    // Lookup buffer type
+    let responseProto = this.protoRoot.lookupType(lookupType);
+    let err = responseProto.verify(message);
+    if (err) console.log('Oh well this got fucked');
+    let payload = responseProto.create(message);
+    let buffer = responseProto.encode(payload).finish();
+    return buffer;
   }
 
   private static checkRegisterBody(
@@ -30,25 +54,33 @@ export default class Authentication {
 
   static POST_register(
     req: express.Request,
-    res: express.Response<AuthResponse>
+    res: express.Response<Uint8Array>
   ) {
     const { username, password } = req.body;
     if (username == '' || password == '') {
-      res
-        .status(400)
-        .json({ success: false, message: 'No password or username given' });
+      res.status(400).send(
+        Authentication.encodeResponse('AuthResponse', {
+          success: false,
+          message: 'No password or username given'
+        })
+      );
       return;
     }
     const check = Authentication.checkRegisterBody(username, password);
     if (!check.success) {
-      res.status(200).json(check);
+      res
+        .status(200)
+        .json(Authentication.encodeResponse('AuthResponse', check));
       return;
     }
     Database.mongo.userExists(username).then((exists) => {
       if (exists) {
-        res
-          .status(200)
-          .json({ success: false, message: 'Username already exists' });
+        res.status(200).json(
+          Authentication.encodeResponse('AuthResponse', {
+            success: false,
+            message: 'Username already exists'
+          })
+        );
       } else {
         let { hashedPassword, salt } = saltPassword(password);
         let userData: UserDataSchema = {
@@ -59,13 +91,17 @@ export default class Authentication {
           token: 'photon_cock'
         };
         Database.mongo.register(userData);
-        res.status(200).json({ success: true });
+        res
+          .status(200)
+          .json(
+            Authentication.encodeResponse('AuthResponse', { success: true })
+          );
       }
       return;
     });
   }
 
-  static POST_login(req: express.Request, res: express.Response<AuthResponse>) {
+  static POST_login(req: express.Request, res: express.Response<Uint8Array>) {
     const username = req.body.username;
 
     // We technically don't need to check if the username exists since
@@ -77,34 +113,44 @@ export default class Authentication {
           // Create a token for the logged in user and save it
           let newToken = random.generateString(64);
           Database.mongo.login(username, newToken).then(() => {
-            res.status(200).json({ success: true, token: newToken });
+            res.status(200).json(
+              Authentication.encodeResponse('AuthResponse', {
+                success: true,
+                token: newToken
+              })
+            );
           });
         } else {
-          res.status(200).json({
-            success: false,
-            message: 'Check username/password combination'
-          });
+          res.status(200).json(
+            Authentication.encodeResponse('AuthResponse', {
+              success: false,
+              message: 'Check username/password combination'
+            })
+          );
         }
       });
   }
 
-  static POST_logout(
-    req: express.Request,
-    res: express.Response<AuthResponse>
-  ) {
+  static POST_logout(req: express.Request, res: express.Response<Uint8Array>) {
     const { token } = req.body;
     Database.mongo
       .getUserFromToken(token)
       .then((userData) => {
         Database.mongo.logout(userData.username).then(() => {
-          res.status(200).json({ success: true });
+          res
+            .status(200)
+            .json(
+              Authentication.encodeResponse('AuthResponse', { success: true })
+            );
         });
       })
       .catch((err) => {
-        res.status(403).json({
-          success: false,
-          message: 'Token missmatch, error follows:' + err
-        });
+        res.status(403).json(
+          Authentication.encodeResponse('AuthResponse', {
+            success: false,
+            message: 'Token missmatch, error follows:' + err
+          })
+        );
       });
   }
 }
